98ed2a0844f83c3f2d941022d76a1d86be70218f
Christian Gawron <christian.gawron@de.ibm.com>
2018-01-08 14:41:12 +0100

    added live site

---
+++ docs/main.bundle.js
@@ -0,0 +1,374 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div>\n    <h1>\n        Wilkommen beim {{ title }}!\n    </h1>\n    <app-meals></app-meals>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Mensaplan';
+    }
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("../../../../../src/app/app.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__meals_meals_component__ = __webpack_require__("../../../../../src/app/meals/meals.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__meal_details_meal_details_component__ = __webpack_require__("../../../../../src/app/meal-details/meal-details.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__meals_service__ = __webpack_require__("../../../../../src/app/meals.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_4__meals_meals_component__["a" /* MealsComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__meal_details_meal_details_component__["a" /* MealDetailsComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */]
+            ],
+            providers: [
+                __WEBPACK_IMPORTED_MODULE_6__meals_service__["a" /* MealsService */]
+            ],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/meal-details/meal-details.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "table {\n    width: 80%;\n}\n\ntd {\n    text-align: initial;\n}\n\ntd.label {\n    width: 5em;\n}\n\ntd input {\n    width: 30em;\n}\n\ntd img {\n    height: 24px;\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/meal-details/meal-details.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div *ngIf=\"meal\">\n    <h2>Details zum Gericht</h2>\n    <table>\n        <tr>\n            <td class=\"label\">Menü:</td>\n            <td><img src=\"assets/icon-menue-{{meal.id}}.png\" /></td>\n        </tr>\n        <tr>\n            <td class=\"label\">Gericht:</td>\n            <td><input [(ngModel)]=\"meal.name\" /></td>\n        </tr>\n        <tr>\n            <td class=\"label\">Art:</td>\n            <td>\n                <select [(ngModel)]=\"meal.art\">\n            <option *ngFor=\"let art of ['rind', 'schwein', 'vegetarisch']\" [ngValue]=\"art\">{{art}}</option>\n          </select>\n            </td>\n        </tr>\n    </table>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/meal-details/meal-details.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MealDetailsComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__meal__ = __webpack_require__("../../../../../src/app/meal.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var MealDetailsComponent = (function () {
+    function MealDetailsComponent() {
+    }
+    MealDetailsComponent.prototype.ngOnInit = function () {
+    };
+    __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["z" /* Input */])(),
+        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_1__meal__["a" /* Meal */])
+    ], MealDetailsComponent.prototype, "meal", void 0);
+    MealDetailsComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-meal-details',
+            template: __webpack_require__("../../../../../src/app/meal-details/meal-details.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/meal-details/meal-details.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], MealDetailsComponent);
+    return MealDetailsComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/meal.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Meal; });
+var Meal = (function () {
+    function Meal() {
+    }
+    return Meal;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/meals.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MealsService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__mock_meals__ = __webpack_require__("../../../../../src/app/mock-meals.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var MealsService = (function () {
+    function MealsService() {
+    }
+    MealsService.prototype.getMeals = function () {
+        return __WEBPACK_IMPORTED_MODULE_1__mock_meals__["a" /* MEALS */];
+    };
+    MealsService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [])
+    ], MealsService);
+    return MealsService;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/meals/meals.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".selected {\n    background-color: #CFD8DC !important;\n    color: white;\n}\n\n.meals {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 80%;\n}\n\n.meals table {\n    border-collapse: collapse;\n    border-spacing: 0;\n    padding: 10px;\n    border: 4px;\n}\n\n.meals tr {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border: 0px;\n    border-radius: 4px;\n}\n\n.meals tr.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n}\n\n.meals tr:hover {\n    color: #607D8B;\n    background-color: #DDD;\n}\n\n.meals td {\n    padding: 0.8em;\n}\n\ntd.left {\n    text-align: left;\n}\n\n.meals td img {\n    height: 24px;\n}\n\n.meals .text {\n    position: relative;\n    top: -3px;\n}\n\n.meals .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/meals/meals.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<table class=\"meals\">\n    <tr>\n        <th>Menü</th>\n        <th class=\"left\">Gericht</th>\n        <th>Art</th>\n    </tr>\n    <tr *ngFor=\"let meal of meals\" [class.selected]=\"meal === selectedMeal\" (click)=\"onSelect(meal)\">\n        <td><img src=\"assets/icon-menue-{{meal.id}}.png\" /></td>\n        <td class=\"left\">{{meal.name}}</td>\n        <td><img src=\"assets/icon-{{meal.art}}.png\" /></td>\n    </tr>\n</table>\n\n<app-meal-details [meal]=\"selectedMeal\"></app-meal-details>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/meals/meals.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MealsComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__meals_service__ = __webpack_require__("../../../../../src/app/meals.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var MealsComponent = (function () {
+    function MealsComponent(mealsService) {
+        this.mealsService = mealsService;
+    }
+    MealsComponent.prototype.onSelect = function (meal) {
+        this.selectedMeal = meal;
+    };
+    MealsComponent.prototype.ngOnInit = function () {
+        this.meals = this.mealsService.getMeals();
+    };
+    MealsComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-meals',
+            template: __webpack_require__("../../../../../src/app/meals/meals.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/meals/meals.component.css")]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__meals_service__["a" /* MealsService */]])
+    ], MealsComponent);
+    return MealsComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/mock-meals.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MEALS; });
+var MEALS = [
+    { id: '1', name: 'Rindergeschnetzeltes mit Pilzen', art: 'rind' },
+    { id: '2', name: 'Schäufelebraten mit Bratensauce', art: 'schwein' },
+    { id: 'tagesgericht', name: 'Penne Nudeln mit Mango-Peperoni-Sauce', art: 'vegetarisch' }
+];
+
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
